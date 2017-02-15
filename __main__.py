792dd61ad957eb202f158659aeb08855e67320cb
PrashantPal <pal.prashant31@gmail.com>
2017-02-15 14:18:38 +0530

    parser script v2

--- __main__.py
+++ __main__.py
@@ -6,29 +6,36 @@ from exporter.report_card_exporters import ReportCardExporter
 from exporter.consolidated_exporter import ConsolidatedExporter
 from utils.name_diff import get_name_list
 from utils.cell_functions import find_cell
+from parsers.DasboardParserV2 import DashboardParserV2
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         print("Please supply valid options")
     elif sys.argv[1] == 'parse':
-        for file in os.listdir('data1'):
+        for file in os.listdir('data'):
             if 'xls' in file[-4:]:
                 print(file)
-                workbook = open_workbook('data1/'+file)
+                workbook = open_workbook('data/'+file)
                 worksheet = workbook.sheet_by_index(0)
                 dparser = DashboardParser(worksheet)
                 dparser.save_dashboard()
+    elif sys.argv[1] == 'parse2':
+        for file in os.listdir('data'):
+            if 'xls' in file[-4:]:
+                print(file)
+                DashboardParserV2('data/{}'.format(file))
     elif sys.argv[1] == 'diff':
         name_list = []
         for file in os.listdir('data'):
-            try:
-                workbook = open_workbook('data/'+file, formatting_info=True)
-                temp_list = get_name_list(workbook, 1)
-                for x in temp_list:
-                    if x not in name_list:
-                        name_list.append(x)
-            except NotImplementedError:
-                print("Formatting not implemented for this file. Convert xlsx to xls")
+            if 'xls' == file[-3:] and '~' != file[0]:
+                try:
+                    workbook = open_workbook('data/'+file, formatting_info=True)
+                    temp_list = get_name_list(workbook, 1)
+                    for x in temp_list:
+                        if x not in name_list:
+                            name_list.append(x)
+                except NotImplementedError:
+                    print("Formatting not implemented for this file. Convert xlsx to xls")
         print(name_list)
     elif sys.argv[1] == 'reportcard':
         exporter = ReportCardExporter()
