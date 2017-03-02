3d5aaa2a8b1abc7b806fd2a2ec831fa034ed502e
Prashant <pal.prashant31@gmail.com>
2017-03-02 16:31:47 +0530

    Flask Api

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify, abort
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return "Hello, World!"
+
+
+@app.route('/dashboard', methods=['POST'])
+def dashboard_update():
+    file = request.files['uploadfile']
+    print(file.filename)
+    return jsonify({'file': file.filename}), 201
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
