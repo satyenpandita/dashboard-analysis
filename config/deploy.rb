c65440beb62674654defcef9313919f76a81aafa
Prashant <pal.prashant31@gmail.com>
2017-03-09 14:17:06 +0530

    cap

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,11 +1,12 @@
 # config valid only for current version of Capistrano
 lock "3.7.2"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "auroville"
+set :branch, "flask_api"
+set :repo_url, "git@github.com:Prashant31/dashboard-analysis.git"
 
 # Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+# ask :flask_api, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to, "/var/www/my_app_name"
@@ -18,7 +19,7 @@ set :repo_url, "git@example.com:me/my_repo.git"
 # set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
 
 # Default value for :pty is false
-# set :pty, true
+set :pty, true
 
 # Default value for :linked_files is []
 # append :linked_files, "config/database.yml", "config/secrets.yml"
