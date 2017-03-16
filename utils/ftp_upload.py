320cee486804f9eea9af70ee4fd1c7d477f90d7f
Prashant <pal.prashant31@gmail.com>
2017-03-16 15:58:33 +0530

    credentials

--- utils/ftp_upload.py
+++ utils/ftp_upload.py
@@ -1,15 +1,25 @@
-from ftplib import FTP
-
+import paramiko
+import json
+from os.path import abspath, exists
 
 def ftp_upload(file_path):
     try:
-        output_path = file_path
-        session = FTP('sftp.bloomberg.com', 'u97335997', 'rn]dT[27OQOE=-k8')
-        file = open(output_path, 'rb')  # file to send
-        session.storbinary('portfolio.xlsx', file)  # send the file
-        file.close()  # close file and FTP
-        session.quit()
+        username, password = get_credentials()
+        transport = paramiko.Transport(('sftp.bloomberg.com', 22))
+        transport.connect(username=username, password=password)
+        sftp = paramiko.SFTPClient.from_transport(transport)
+        sftp.put(file_path, "/", confirm=True)
+        sftp.close()
+        transport.close()
+        print('Upload done.')
     except FileNotFoundError as e:
         print(str(e))
     except Exception as e:
         print(str(e))
+
+
+def get_credentials():
+    with open("utils/secrets.json") as data_file:
+        data = json.load(data_file)
+        credentials = data["ftp_credentials"]
+        return credentials['username'], credentials['password']
