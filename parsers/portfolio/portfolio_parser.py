d8cdafa9686cd796a7d21f92a8da66f3ffdca8ef
PrashantPal <pal.prashant31@gmail.com>
2017-03-28 15:13:45 +0530

    invalid tickers plus analyst name

--- parsers/portfolio/portfolio_parser.py
+++ parsers/portfolio/portfolio_parser.py
@@ -1,7 +1,11 @@
+import re
 from utils.cell_functions import find_cell, cell_value
 from exporter.portfolio.portfolio_exporter import PortfolioExporter
 from utils.ftp_upload import ftp_upload, get_users
 from utils.email_sender import send_mail
+
+
+INVALID_TICKERS = []
 
 
 def get_tickers(worksheet, direction):
@@ -24,9 +28,20 @@ def get_tickers(worksheet, direction):
                     else:
                         break
             if direction == 'long' or (direction == 'short' and short):
-                folio[cell_value(worksheet, next_row, col)] = cell_value(worksheet, next_row, col+1), \
+                if is_valid_ticker(val):
+                    folio[val] = cell_value(worksheet, next_row, col+1), \
                                                               cell_value(worksheet, next_row, col+2)
+                else:
+                    INVALID_TICKERS.append(val)
     return folio
+
+
+def is_valid_ticker(ticker):
+    sr = re.search(r"\s[A-Za-z]{2}\s(Equity)$", ticker)
+    if sr is None:
+        return False
+    else:
+        return True
 
 
 def get_analyst(worksheet):
@@ -34,7 +49,7 @@ def get_analyst(worksheet):
     user_data = get_users()
     if name:
         for key, val in user_data.items():
-            if name.lower() in val:
+            if name.strip().lower() in val:
                 return key
     return ""
 
@@ -44,6 +59,7 @@ class PortfolioParser(object):
     def __init__(self, worksheet):
         self.long_tickers = get_tickers(worksheet, "long")
         self.short_tickers = get_tickers(worksheet, "short")
+        self.invalid_tickers = INVALID_TICKERS
         self.analyst = get_analyst(worksheet)
         self.output_file_long = ''
         self.output_file_long_name = ''
@@ -56,12 +72,22 @@ class PortfolioParser(object):
         self.output_file_short, self.output_file_short_name = exporter.export(self.analyst, 'short')
 
     def send_email(self):
-        send_mail("ppal@auroim.com", ["ppal@auroim.com", "bmukherjee@auroim.com"], "Best Ideas Published",
-                  "Please file the file attached published by {}".format(self.analyst),
+        send_mail("ppal@auroim.com", ["datascience@auroim.com"], "Best Ideas Published",
+                  self.get_body(),
                   [self.output_file_long, self.output_file_short], username="ppal@auroim.com", password="AuroOct2016")
+        global INVALID_TICKERS
+        INVALID_TICKERS = []
 
     def ftp_upload(self):
         output_path = self.output_file_long if self.output_file_long else r'uploaded_files/output/portfolio.xlsx'
         ftp_upload(output_path, self.output_file_long_name)
         output_path = self.output_file_short if self.output_file_short else r'uploaded_files/output/portfolio.xlsx'
         ftp_upload(output_path, self.output_file_short_name)
+
+    def get_body(self):
+        if len(self.invalid_tickers) > 0:
+            return """Please find the Best Ideas files published by {} \n\n Some Invalid Tickers Found \n {}""".\
+                format(self.analyst, "\n".join(self.invalid_tickers))
+        else:
+            return """Please find the Best Ideas files published by {} \n\n No Invalid Securities""".\
+                format(self.analyst)
