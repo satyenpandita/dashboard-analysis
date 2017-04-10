f2ce27934133703e4cca5bb9c0e293eb128addfc
PrashantPal <pal.prashant31@gmail.com>
2017-04-10 16:09:15 +0530

    file split + bug fixes

--- config/celery.py
+++ config/celery.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import, unicode_literals
 from celery import Celery, task
 
-app = Celery(broker='redis://localhost:6379/0', include=['utils.upload_ops'])
+app = Celery(broker='redis://localhost:6379/0', include=['utils.upload_ops', 'utils.email_sender'])
 
 if __name__ == '__main__':
     app.start()
