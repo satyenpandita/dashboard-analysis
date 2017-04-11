28e5a95b35d19e9ab3d2c7bac21ec2f3870d6941
PrashantPal <pal.prashant31@gmail.com>
2017-04-11 17:11:31 +0530

    bug fixes

--- models/CumulativeDashBoard.py
+++ models/CumulativeDashBoard.py
@@ -4,11 +4,12 @@ from models.BaseModel import BaseModel
 
 class CumulativeDashBoard(BaseModel):
     def __init__(self, stock_code, base, bull, bear):
+        now = datetime.datetime.now()
         self.stock_code = stock_code
         self.base = base
         self.bull = bull
         self.bear = bear
-        self.created_at = datetime.datetime.today().date()
+        self.created_at = now.strftime('%m/%d/%y')
 
     @classmethod
     def from_dict(cls, obj):
