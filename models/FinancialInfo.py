8c4e4d9984df8971541c990a20e6ce13c900a77b
PrashantPal <pal.prashant31@gmail.com>
2017-04-13 22:32:43 +0530

    minor fixes

--- models/FinancialInfo.py
+++ models/FinancialInfo.py
@@ -1,4 +1,4 @@
-from utils.cell_functions import cell_value_by_key
+from utils.cell_functions import cell_value_by_key, find_cell, cell_value
 
 
 class FinancialInfo:
@@ -6,10 +6,14 @@ class FinancialInfo:
     def __init__(self, worksheet):
         super(FinancialInfo, self).__init__()
         self.fd_shares = cell_value_by_key(worksheet, 'FD Shares (m):')
-        self.price_per_share = cell_value_by_key(worksheet, 'Price (USD/sh):')
+        cell_address = find_cell(worksheet, "Mkt Cap (M):")
         self.market_cap = None
-        if self.fd_shares and self.price_per_share:
-            self.market_cap = self.fd_shares*self.price_per_share
+        self.price_per_share = None
+        if cell_address:
+            row, col = cell_address
+            self.price_per_share = cell_value(worksheet, row - 1, col + 1)
+            self.market_cap = cell_value(worksheet, row, col + 1)
+
         self.cash = cell_value_by_key(worksheet, '  - Cash')
         self.debt = cell_value_by_key(worksheet, '  + Debt')
         self.others_adj = cell_value_by_key(worksheet, '  + Other Adj')
